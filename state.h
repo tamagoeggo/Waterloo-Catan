908d000d242a6f4c5710afe76ed506026dbf3d9b
rikwan <rikwan@student.cs.uwaterloo.ca>
2019-12-01 16:10:15 -0500

    Inclusion for state

--- state.h
+++ state.h
@@ -1,6 +1,9 @@
 #ifndef _STATE_H_
 #define _STATE_H_
 
+#include "player.h"
+#include "type.h"
+
 struct State {
 	Player player; // player color
 	Type type; // achievement, assignment, midterm, exam or none
