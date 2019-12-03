175b712255e1fbf3c1f36a94c4867d0a00346b38
AmandaZhu360 <amandazhu361@gmail.com>
2019-12-03 17:45:58 -0500

    board dtor

--- board.h
+++ board.h
@@ -40,13 +40,7 @@ class Board {
 	public:
 
 	Board(bool enhance, std::string board, int layer = 2); // ctor with randomized resources and value, init textdisplay
-
-	/*
-	Board(const Board&) = delete; // disable copy ctor
-  Board& operator=(const Board&) = delete; // disable copy assignment
-  ~Board() = default;
-	*/
-
+	~Board(); // dtor
 	void completeCriterion(const int coordinate, Player player); // places criteria(similar to setPiece in a4q4) second turn onward
 	void firstCriterion(const int coordinate, Player player);
 	void upgradeCriterion(const int coordinate, Player player);
