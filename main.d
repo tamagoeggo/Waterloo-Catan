52f4c62dc1c016fc773544f1ccdfbdeccfd95a82
tttzach <tttzach@gmail.com>
2019-12-03 01:39:16 -0500

    Fixed save

--- main.d
+++ main.d
@@ -1,3 +1,3 @@
-main.o: main.cc board.h tile.h resource.h criterion.h subject.h state.h \
- player.h type.h student.h goal.h textdisplay.h observer.h fair.h dice.h \
- loaded.h
+main.o: main.cc board.h tile.h resource.h player.h criterion.h subject.h \
+  state.h type.h student.h goal.h textdisplay.h observer.h fair.h dice.h \
+  loaded.h
--- student.d
+++ student.d
@@ -1,2 +1,2 @@
-student.o: student.cc student.h player.h resource.h type.h state.h \
- criterion.h subject.h
+student.o: student.cc student.h player.h resource.h type.h state.h goal.h \
+  subject.h criterion.h
