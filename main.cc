5beac0bceab706ef7160d5cb72e1367fbc31d182
AmandaZhu360 <amandazhu361@gmail.com>
2019-12-03 03:46:53 -0500

    eof

--- main.cc
+++ main.cc
@@ -364,7 +364,7 @@ int main(int argc, char* argv[]) {
 			}
 		}
 		// if eof
-		catch(ios::failure &){
+		catch(std::__ios_failure){
 			b.saveGame(whoseTurn);
 			cout << "Game ended unexpectedly, saving game." << endl;
 		}
